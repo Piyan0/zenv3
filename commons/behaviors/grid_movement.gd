e15deb0085a69f874ba36964e368331c9f7c6bd5
Piyan0 <piyanapriyanto.0@gmail.com>
2026-04-23 22:04:45 +0700

    m

--- commons/behaviors/grid_movement.gd
+++ commons/behaviors/grid_movement.gd
@@ -1,25 +1,52 @@
 class_name GridMovement
 extends Node2D
 
-@export var unit_size= 16
-@export var target: Node2D
-@export var speed= 10
-@export var enabled_input= true
+@export var tile_size = 16
+@export var target: CharacterBody2D
+@export var speed = 60 # Pixel per detik
 
-var _routes= []
-var _current_routes: Array[Vector2]
-var _current_dir: Vector2
-var _current_start_pos: Vector2
-var _is_moving= false
+var _pixel_moved = 0
+var _direction = Vector2.ZERO
+var _is_process_walk = false
+var _step_size = 0.0
+var _step = 0.0
 
+func _ready():
+    # Jika speed 60, maka _step_size = 0.0166 detik per pixel
+    _step_size = 1.0 / speed
 
 func _physics_process(delta: float) -> void:
-    pass
+    _update_direction()
+    _process_movement(delta)
 
+func _update_direction():
+    if _is_process_walk: return
+    
+    var input_dir = Vector2.ZERO
+    if Input.is_action_pressed("ui_up"): input_dir = Vector2.UP
+    elif Input.is_action_pressed("ui_down"): input_dir = Vector2.DOWN
+    elif Input.is_action_pressed("ui_left"): input_dir = Vector2.LEFT
+    elif Input.is_action_pressed("ui_right"): input_dir = Vector2.RIGHT
+    
+    if input_dir != Vector2.ZERO:
+        _direction = input_dir
 
 func _process_movement(delta):
-    pass
-
-
-func _get_time():
-    return float(unit_size) / speed
+    if _direction == Vector2.ZERO: return
+    
+    _is_process_walk = true
+    _step += delta
+    
+    # "while" akan memproses semua pixel yang seharusnya jalan selama waktu delta
+    # Ini kunci biar gak stutter pas FPS drop di bawah 24
+    while _step >= _step_size and _is_process_walk:
+        target.move_and_collide(_direction)
+        _step -= _step_size
+        _pixel_moved += 1
+        
+        # Jika sudah menempuh 1 kotak (16px)
+        if _pixel_moved >= tile_size:
+            _pixel_moved = 0
+            _step = 0 # Reset sisa waktu agar tidak kebablasan ke kotak berikutnya
+            _direction = Vector2.ZERO
+            _is_process_walk = false
--- levels/demo/main/main.gd
+++ levels/demo/main/main.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 
-@export var spr: Node2D
+@export var spr: CharacterBody2D
 
-func _process(delta: float) -> void:
-    spr.position.x+= 60 * delta
+func _physics_process(delta: float) -> void:
+    if Engine.get_frames_drawn() < 100: return
+    #print(Engine.get_frames_drawn())
+    spr.move_and_collide(Vector2.RIGHT * 1)
