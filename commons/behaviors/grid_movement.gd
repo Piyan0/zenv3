a375dbeed8ae5aa6d2113e38ddd0a6da362e3595
Piyan0 <piyanapriyanto.0@gmail.com>
2026-04-23 23:36:52 +0700

    m

--- commons/behaviors/grid_movement.gd
+++ commons/behaviors/grid_movement.gd
@@ -2,51 +2,60 @@ class_name GridMovement
 extends Node2D
 
 @export var tile_size = 16
-@export var target: CharacterBody2D
-@export var speed = 60 # Pixel per detik
+@export var target: Node2D
+@export var speed= 60
 
-var _pixel_moved = 0
+var _move_progress= 0.0
+var _is_moving= false
 var _direction = Vector2.ZERO
-var _is_process_walk = false
-var _step_size = 0.0
-var _step = 0.0
+var _destination_pos
+var _start_pos
 
-func _ready():
-    # Jika speed 60, maka _step_size = 0.0166 detik per pixel
-    _step_size = 1.0 / speed
+func _ready() -> void:
+    pass
+
 
 func _physics_process(delta: float) -> void:
-    _update_direction()
-    _process_movement(delta)
+    _update_input()
+    _update_destination()
+    _process_moving(delta)
 
-func _update_direction():
-    if _is_process_walk: return
-    
-    var input_dir = Vector2.ZERO
-    if Input.is_action_pressed("ui_up"): input_dir = Vector2.UP
-    elif Input.is_action_pressed("ui_down"): input_dir = Vector2.DOWN
-    elif Input.is_action_pressed("ui_left"): input_dir = Vector2.LEFT
-    elif Input.is_action_pressed("ui_right"): input_dir = Vector2.RIGHT
-    
-    if input_dir != Vector2.ZERO:
-        _direction = input_dir
 
-func _process_movement(delta):
-    if _direction == Vector2.ZERO: return
+func _update_input():
+    if _is_moving:
+        return
+
+    if Input.is_action_pressed("ui_up"):
+        _direction= Vector2.UP
+    elif Input.is_action_pressed("ui_down"):
+        _direction= Vector2.DOWN
+    elif Input.is_action_pressed("ui_left"):
+        _direction= Vector2.LEFT
+    elif Input.is_action_pressed("ui_right"):
+        _direction= Vector2.RIGHT
+    else:
+        _direction= Vector2.ZERO
+
+
+func _update_destination():
+    if _is_moving:
+        return
+    _start_pos= target.position
+    _destination_pos= target.position + (Vector2(tile_size, tile_size) * _direction)
+
+func _process_moving(delta):
+    if _direction== Vector2.ZERO: return
+
+    _is_moving= true
+    var time= _get_time(speed)
+    _move_progress+= delta / time
+    target.position= lerp(_start_pos, _destination_pos, _move_progress)
+
+    if _move_progress >= 1:
+        _is_moving= false
+        _move_progress= 0
+
+
+func _get_time(p_speed):
+    return tile_size / float(p_speed)
     
-    _is_process_walk = true
-    _step += delta
-    
-    # "while" akan memproses semua pixel yang seharusnya jalan selama waktu delta
-    # Ini kunci biar gak stutter pas FPS drop di bawah 24
-    while _step >= _step_size and _is_process_walk:
-        target.move_and_collide(_direction)
-        _step -= _step_size
-        _pixel_moved += 1
-        
-        # Jika sudah menempuh 1 kotak (16px)
-        if _pixel_moved >= tile_size:
-            _pixel_moved = 0
-            _step = 0 # Reset sisa waktu agar tidak kebablasan ke kotak berikutnya
-            _direction = Vector2.ZERO
-            _is_process_walk = false
--- levels/demo/main/main.gd
+++ levels/demo/main/main.gd
@@ -2,8 +2,3 @@ extends Node2D
 
 
 @export var spr: CharacterBody2D
-
-func _physics_process(delta: float) -> void:
-    if Engine.get_frames_drawn() < 100: return
-    #print(Engine.get_frames_drawn())
-    spr.move_and_collide(Vector2.RIGHT * 1)
