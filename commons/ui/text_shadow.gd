b7da0ceed75d569a6918fb680fd389f5ed9a95d1
Piyan0 <piyanapriyanto.0@gmail.com>
2026-04-27 15:58:52 +0700

    m

--- commons/ui/text_shadow.gd
+++ commons/ui/text_shadow.gd
@@ -8,10 +8,11 @@ var clone
 
 func _ready():
     clone= target.duplicate()
-    target.add_child.call_deferred(clone)
+    target.add_child(clone)
     clone.self_modulate= Color.WHITE
     clone.modulate= shadow_color
     clone.show_behind_parent= true
+    clone.set_anchors_and_offsets_preset(Control.LayoutPreset.PRESET_FULL_RECT)
     clone.position= Vector2.ZERO + offset
     
 
@@ -19,5 +20,6 @@ func _process(_delta):
     if clone:
         clone.visible_characters= target.visible_characters
         clone.text= target.text
+        # printt(clone.visible_characters, target.visible_characters)
         
     
--- ui/dialogue/dialogue.gd
+++ ui/dialogue/dialogue.gd
@@ -2,11 +2,27 @@ extends CanvasLayer
 
 @export var bg: Control
 @export var container: Control
+@export var lb_name: Label
+@export var lb_msg: Label
+var _dialogue_base: DialogueBase
 
 func _ready() -> void:
     # var x= await AnimateOpenCenter.spawn(bg, 0.4, func(): container.hide(), func(): container.show())
-    pass
+    _dialogue_base= DialogueBase.new() 
+    _dialogue_base.on_progress= func(d, v, j):
+        lb_name.text= d.speaker
+        lb_msg.text= d.msg
+        lb_msg.visible_characters= v
+    
+    _dialogue_base.dialogue_batch= [
+        DialogueBase.DialogueNormal.new("piyan", "Anjay mabar..."),
+        DialogueBase.DialogueNormal.new("salwa", "keren...."),
+    ]
 
-
+func _input(event: InputEvent):
+    _dialogue_base.input(event)
+    
+    
 func _start_dialogue():
     pass
+    
