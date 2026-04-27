d1062b3dc8158d5b6527bf9991d8ab3335504ffa
Piyan0 <piyanapriyanto.0@gmail.com>
2026-04-27 14:10:03 +0700

    m

--- commons/ui/animate_open_center.gd
+++ commons/ui/animate_open_center.gd
@@ -3,34 +3,40 @@ extends Node
 
 signal finished()
 
-var parent: Node
 var target: Control
 var duration
 
 
-func _init(p_parent, p_target, p_duration= 0.2):
-    parent= p_parent
-    target= p_target
-    duration= p_duration
+func _ready() -> void:
+    await target.get_tree().process_frame
     _animate()
 
 
+static func spawn(p_target, p_duration= 0.2, on_start= func():pass, on_end= func(): pass):
+    var instance= AnimateOpenCenter.new()
+    instance.target= p_target
+    instance.duration= p_duration
+    Bootstrap.add_child.call_deferred(instance)
+    # sometime _ready have to do initial calculation for ui, if we for example hide some ui before it initial frame,
+    # when we access the size, it will be 0,0 (not processed by layout engine).
+    on_start.call_deferred()
+    await instance.finished
+    on_end.call()
+
+
 func _animate():
-    target.modulate= Color.TRANSPARENT
-    var clone= target.duplicate()
-    
-    clone.top_level= true
+    var clone= target.duplicate()    
+    clone.name= "OpenFromCenterNode"
     clone.size= target.size
     clone.global_position= target.global_position
     clone.modulate= Color.WHITE
-    
     var half_size= target.size.y / 2
     var start_size_y= 0
     var end_size_y= target.size.y
     var start_pos= clone.global_position + Vector2(0, half_size)
-    var end_pos= target.position
+    var end_pos= target.global_position
     
-    parent.add_child.call_deferred(clone)
+    Bootstrap.canvas.add_child.call_deferred(clone)
     await clone.ready
     
     clone.size.y= start_size_y
@@ -41,9 +47,7 @@ func _animate():
     t.tween_property(clone, "size:y", end_size_y, duration)
     
     await t.finished
-
     finished.emit()
-    target.modulate= Color.WHITE
     clone.free()
     queue_free()
     
---
+++ ui/dialogue/dialogue.gd
@@ -0,0 +1,12 @@
+extends CanvasLayer
+
+@export var bg: Control
+@export var container: Control
+
+func _ready() -> void:
+    # var x= await AnimateOpenCenter.spawn(bg, 0.4, func(): container.hide(), func(): container.show())
+    pass
+
+
+func _start_dialogue():
+    pass
