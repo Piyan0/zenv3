da8946280d5bd7150ddcfd25fecf90a5c8628c33
Piyan0 <piyanapriyanto.0@gmail.com>
2026-05-05 14:21:03 +0700

    m

--- commons/spawnable_animation.gd
+++ commons/spawnable_animation.gd
@@ -12,7 +12,7 @@ var _anim_process
 
 func _ready():
     _spr = _add_sprite2d()
-    _spr.global_position = spawn_pos
+    position = spawn_pos
     _anim_process = AnimationProcess.new()
     _anim_process.target = _spr
     _anim_process.animation_data = animation_data
@@ -23,12 +23,13 @@ func _ready():
     add_child(_anim_process)
 
 
-static func spawn(p_animation_data, p_pos = Vector2.ZERO):
+static func spawn(p_animation_data, p_pos = Vector2.ZERO, p_z_index = 10):
     var instance = SpawnableAnimation.new()
+    instance.z_index = p_z_index
     instance.animation_data = p_animation_data
     instance.spawn_pos = p_pos
     instance.name = "SpawnableAnimation"
-    Bootstrap.world_canvas.add_child(instance)
+    Engine.get_main_loop().current_scene.add_child(instance)
     await instance.animation_finished
 
 
--- levels/demo/main/main.gd
+++ levels/demo/main/main.gd
@@ -6,4 +6,7 @@ extends Map
 
 func _ready():
     super._ready()
-    SpawnableAnimation.spawn(Bootstrap.asset_loader.get_asset("anim_phone"), spr.global_position)
+    print(spr.z_index)
+    spr.hide()
+    await SpawnableAnimation.spawn(Bootstrap.asset_loader.get_asset("anim_phone"), spr.global_position)
+    spr.show()
